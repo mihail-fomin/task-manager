c444315d59cc74443d8b8b7823a1b49eec8f7e65
misha133kv <mihail_fomin_95@mail.ru>
2023-12-23 13:44:34 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import GlobalStylesProvider from './providers/GlobalStylesProvider'
+import Sidebar from './Sidebar/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalStylesProvider>
+          <Sidebar />
+          {children}
+        </GlobalStylesProvider>
+        </body>
     </html>
   )
 }
