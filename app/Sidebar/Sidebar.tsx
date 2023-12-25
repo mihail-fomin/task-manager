088c34c3d61182524854c724eee54e9f1324d5c1
misha133kv <mihail_fomin_95@mail.ru>
2023-12-25 22:47:11 +0500

    added profile styles for sidebar

--- app/Sidebar/Sidebar.tsx
+++ app/Sidebar/Sidebar.tsx
@@ -45,6 +45,7 @@ export default function Sidebar() {
           </li>
         })}
       </ul>
+      <button>button</button>
     </SidebarStyled>
   )
 };
@@ -55,4 +56,98 @@ const SidebarStyled = styled.nav`
   background-color: ${(props) => props.theme.colorBg2};
   border: 2px solid ${(props) => props.theme.borderColor2};
   border-radius: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  color: ${(props) => props.theme.colorGrey3};
+
+  .profile {
+    margin: 1.5rem;
+    padding: 1rem 0.8rem;
+    position: relative;
+
+    border-radius: 1rem;
+    cursor: pointer;
+
+    font-weight: 500;
+    color: ${(props) => props.theme.colorGrey0};
+
+    display: flex;
+    align-items: center;
+
+    .profile-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      backdrop-filter: blur(10px);
+      z-index: 0;
+      background: ${(props) => props.theme.colorBg3};
+      transition: all 0.55s linear;
+      border-radius: 1rem;
+      border: 2px solid ${(props) => props.theme.borderColor2};
+
+      opacity: 0.2;
+    }
+
+    h1 {
+      font-size: 1.2rem;
+      display: flex;
+      flex-direction: column;
+
+      line-height: 1.4rem;
+    }
+
+    .image,
+    h1 {
+      position: relative;
+      z-index: 1;
+    }
+
+    .image {
+      flex-shrink: 0;
+      display: inline-block;
+      overflow: hidden;
+      transition: all 0.5s ease;
+      border-radius: 100%;
+
+      width: 70px;
+      height: 70px;
+
+      img {
+        border-radius: 100%;
+        transition: all 0.5s ease;
+      }
+    }
+
+    > h1 {
+      margin-left: 1rem;
+      font-size: clamp(1.2rem 4vw, 1.4rem);
+      line-height: 100%;
+    }
+
+    &:hover {
+      .profile-overlay {
+        opacity: 1;
+        border: 2px solid ${(props) => props.theme.borderColor2};
+      }
+
+      img {
+        transform: scale(1.1);
+      }
+    }
+  }
+
+  .nav-items {
+    position: relative;
+    padding: 0.6rem 1rem 1rem 2.1rem;
+    margin: 0.3rem 0;
+
+    display: grid;
+    grid-template-columns: 40px 1fr;
+    cursor: pointer;
+  }
 `
