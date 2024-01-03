31effd40123437c2cffae82eeab95c77b3440eaa
misha133kv <mihail_fomin_95@mail.ru>
2024-01-03 20:14:13 +0500

    added form function

--- app/Components/Modals/CreateContent.tsx
+++ app/Components/Modals/CreateContent.tsx
@@ -31,64 +31,78 @@ export default function Home() {
     }
   }
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+
+    const task = {
+      title,
+      description,
+      date,
+      completed,
+      important
+    }
+  }
+
   return <>
-    <h1>Create a Task</h1>
-    <div className="input-control">
-      <label htmlFor="title">Title</label>
-      <input
-        type="text"
-        id="title"
-        value={title}
-        name="title"
-        onChange={handleChange("title")}
-        placeholder="Title"
-      />
-    </div>
-    <div className="input-control">
-      <label htmlFor="description">Description</label>
-      <textarea
-        id="description"
-        value={description}
-        name="description"
-        rows={4}
-        onChange={handleChange("description")}
-        placeholder="description"
-      />
-    </div>
-    <div className="input-control">
-      <label htmlFor="description">Date</label>
-      <input
-        type="date"
-        id="date"
-        value={date}
-        name="date"
-        onChange={handleChange(("date"))}
-      />
-    </div>
-    <div className="input-control">
-      <label htmlFor="completed">Toggle Completed</label>
-      <input
-        type="checkbox"
-        id="completed"
-        value={completed.toString()}
-        name="completed"
-        onChange={handleChange(("completed"))}
-      />
-    </div>
-    <div className="input-control">
-      <label htmlFor="completed">Important</label>
-      <input
-        type="checkbox"
-        id="important"
-        value={important.toString()}
-        name="important"
-        onChange={handleChange(("important"))}
-      />
-    </div>
-    <div className="submit-btn">
-      <button type="submit">
-        <span>Submit</span>
-      </button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Create a Task</h1>
+      <div className="input-control">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          name="title"
+          onChange={handleChange("title")}
+          placeholder="Title"
+        />
+      </div>
+      <div className="input-control">
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          value={description}
+          name="description"
+          rows={4}
+          onChange={handleChange("description")}
+          placeholder="description"
+        />
+      </div>
+      <div className="input-control">
+        <label htmlFor="description">Date</label>
+        <input
+          type="date"
+          id="date"
+          value={date}
+          name="date"
+          onChange={handleChange(("date"))}
+        />
+      </div>
+      <div className="input-control">
+        <label htmlFor="completed">Toggle Completed</label>
+        <input
+          type="checkbox"
+          id="completed"
+          value={completed.toString()}
+          name="completed"
+          onChange={handleChange(("completed"))}
+        />
+      </div>
+      <div className="input-control">
+        <label htmlFor="completed">Important</label>
+        <input
+          type="checkbox"
+          id="important"
+          value={important.toString()}
+          name="important"
+          onChange={handleChange(("important"))}
+        />
+      </div>
+      <div className="submit-btn">
+        <button type="submit">
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
   </>
 }
