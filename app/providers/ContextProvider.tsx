6dc718a05bda34227b7b82f7a0a842e1447a5b79
misha133kv <mihail_fomin_95@mail.ru>
2024-01-05 13:43:06 +0500

    added Button

--- app/providers/ContextProvider.tsx
+++ app/providers/ContextProvider.tsx
@@ -1,20 +1,17 @@
 'use client'
 import React from 'react'
 import { GlobalProvider } from '../context/globalProvider'
-import { Toaster } from "react-hot-toast";
-
+import { Toaster } from 'react-hot-toast'
 
 interface Props {
   children: React.ReactNode
 }
 
 export default function ContextProvider({ children }: Props) {
-
-  
   return (
     <GlobalProvider>
       <Toaster />
       {children}
     </GlobalProvider>
-  );
+  )
 }
