572fc5062e7ab16c1740252e4597b32319ec2834
misha133kv <mihail_fomin_95@mail.ru>
2024-01-06 13:43:40 +0500

    styled modal

--- app/Components/Modals/CreateContent.tsx
+++ app/Components/Modals/CreateContent.tsx
@@ -1,9 +1,13 @@
 'use client'
 
-import axios from 'axios'
 import React, { useState } from 'react'
+import axios from 'axios'
 import toast from 'react-hot-toast'
+
+import { useGlobalState } from '@/app/context/globalProvider'
 import styled from 'styled-components'
+import Button from '../Button/Button'
+import { add, plus } from "@/app/utils/Icons";
 
 export default function Home() {
   const [title, setTitle] = useState('')
@@ -11,6 +15,8 @@ export default function Home() {
   const [date, setDate] = useState('')
   const [completed, setCompleted] = useState(false)
   const [important, setImportant] = useState(false)
+
+  const {theme} = useGlobalState()
 
   const handleChange = (name: string) => (e: any) => {
     switch (name) {
@@ -24,10 +30,10 @@ export default function Home() {
         setDate(e.target.value)
         break
       case 'completed':
-        setCompleted(Boolean(e.target.value))
+        setCompleted(e.target.checked)
         break
       case 'important':
-        setImportant(Boolean(e.target.value))
+        setImportant(e.target.checked)
         break
       default:
         break
@@ -61,8 +67,7 @@ export default function Home() {
   }
 
   return (
-    <TaskContentStyled>
-      <form onSubmit={handleSubmit}>
+    <CreateContentStyled onSubmit={handleSubmit} theme ={theme}>
         <h1>Create a Task</h1>
         <div className="input-control">
           <label htmlFor="title">Title</label>
@@ -90,39 +95,123 @@ export default function Home() {
           <label htmlFor="description">Date</label>
           <input type="date" id="date" value={date} name="date" onChange={handleChange('date')} />
         </div>
-        <div className="input-control">
+
+        <div className="input-control toggler">
           <label htmlFor="completed">Toggle Completed</label>
           <input
+            className='custom-checkbox'
             type="checkbox"
             id="completed"
-            value={completed.toString()}
+            checked={completed}
             name="completed"
+            value="yes"
             onChange={handleChange('completed')}
           />
         </div>
-        <div className="input-control">
-          <label htmlFor="completed">Important</label>
+
+        <div className="input-control toggler">
+          <label htmlFor="important">Important</label>
           <input
             type="checkbox"
             id="important"
-            value={important.toString()}
+            checked={important}
             name="important"
             onChange={handleChange('important')}
           />
         </div>
-        <div className="submit-btn">
-          <button type="submit">
-            <span>Submit</span>
-          </button>
-        </div>
-      </form>
-    </TaskContentStyled>
+
+        <div className="submit-btn flex justify-end">
+          <Button
+            type="submit"
+            name="Create Task"
+            icon={add}
+            padding={"0.8rem 2rem"}
+            borderRad={"0.8rem"}
+            fw={"500"}
+            fs={"1.2rem"}
+            background={"rgb(0, 163, 255)"}
+          />
+      </div>
+    </CreateContentStyled>
   )
 }
 
-const TaskContentStyled = styled.div`
-  .input-control input,
-  textarea {
-    color: #000;
+const CreateContentStyled = styled.form`
+  > h1 {
+    font-size: clamp(1.2rem, 5vw, 1.6rem);
+    font-weight: 600;
   }
-`
+
+  color: ${(props) => props.theme.colorGrey1};
+
+  .input-control {
+    position: relative;
+    margin: 1.6rem 0;
+    font-weight: 500;
+
+    @media screen and (max-width: 450px) {
+      margin: 1rem 0;
+    }
+
+    label {
+      margin-bottom: 0.5rem;
+      display: inline-block;
+      font-size: clamp(0.9rem, 5vw, 1.2rem);
+
+      span {
+        color: ${(props) => props.theme.colorGrey3};
+      }
+    }
+
+    input,
+    textarea {
+      width: 100%;
+      padding: 1rem;
+
+      resize: none;
+      background-color: ${(props) => props.theme.colorGreyDark};
+      color: ${(props) => props.theme.colorGrey2};
+      border-radius: 0.5rem;
+    }
+  }
+
+  .submit-btn button {
+    transition: all 0.35s ease-in-out;
+
+    @media screen and (max-width: 500px) {
+      font-size: 0.9rem !important;
+      padding: 0.6rem 1rem !important;
+
+      i {
+        font-size: 1.2rem !important;
+        margin-right: 0.5rem !important;
+      }
+    }
+
+    i {
+      color: ${(props) => props.theme.colorGrey0};
+    }
+
+    &:hover {
+      background: ${(props) => props.theme.colorPrimaryGreen} !important;
+      color: ${(props) => props.theme.colorWhite} !important;
+    }
+  }
+
+  .toggler {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    cursor: pointer;
+
+    label {
+      flex: 1;
+    }
+
+    input {
+      width: initial;
+    }
+  }
+`;
+
